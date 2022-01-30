ce374024b84136d16529789e5bfedfc720292f88
Marcin <marcinrozkwitalski98@gmail.com>
2022-01-30 20:09:15 +0100

    Created entry logic for login

---
+++ ARC2_Projekt/Assets/Scripts/Login-Register/LoginUser.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class LoginUser : MonoBehaviour
+{
+    public InputField usernameInput;
+    public InputField passwordInput;
+    public Button loginButton;
+    public Text loginButtonText;
+
+    public void Login(){
+        loginButton.interactable = false;
+        if(usernameInput.text.Length < 3){
+            ErrorOnLoginMessage("Check username");
+        }
+        else if (passwordInput.text.Length < 3){
+            ErrorOnLoginMessage("Check password");
+        } 
+        else {
+            Debug.Log("Good to go.");
+        }
+    }
+
+    public void ErrorOnLoginMessage(string message){
+        loginButton.GetComponent<Image>().color = Color.red;
+        loginButtonText.text = message;
+        loginButtonText.fontSize = 60;
+    }
+
+    public void ResetLoginButton(){
+        loginButton.GetComponent<Image>().color = Color.white;
+        loginButtonText.text = "Login";
+        loginButtonText.fontSize = 70;
+        loginButton.interactable = true;
+    }
+
+}
