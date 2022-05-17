f9e6269cbaf5688b9564eb2a1ed0facf7518b028
TrojanFi <s20088@pjwstk.edu.pl>
2022-05-17 23:41:13 +0200

    inventory update
    You can Filtr cards - attack defence all

--- ARC2_Projekt/Assets/Scripts/Player/Player.cs
+++ ARC2_Projekt/Assets/Scripts/Player/Player.cs
@@ -10,16 +10,24 @@ public class Player : MonoBehaviour
     public int CurrentPlayerId;
     public GameObject playerCardPrefab;
     public GameObject panel;
+    public GameObject deckPanel;
     void Start()
     {
         var CurrentPlayer = GameObject.FindGameObjectWithTag("CurrentPlayer");
         CurrentPlayerId = CurrentPlayer.GetComponent<CurrentPlayer>().Id;
         StartCoroutine(GetAllPlayerCards());
+        // Screem.width do skalowania na inne ekrany
     }
 
     public void LoadPlayerWelcomeScene()
     {
         FindObjectOfType<SceneSwitcher>().LoadPlayerWelcomeScene();
+    }
+
+    public void AllCards()
+    {
+        DestroyAllPlayerCards();
+        StartCoroutine(GetAllPlayerCards());
     }
 
     IEnumerator GetAllPlayerCards()
@@ -41,6 +49,8 @@ public class Player : MonoBehaviour
                 playerCard.GetComponent<PlayerCard>().description = player_cards[2];
                 playerCard.GetComponent<PlayerCard>().price = int.Parse(player_cards[3]);
                 playerCard.GetComponent<PlayerCard>().points = int.Parse(player_cards[4]);
+                playerCard.GetComponent<PlayerCard>().healthPoints = int.Parse(player_cards[5]);
+                playerCard.GetComponent<PlayerCard>().id = int.Parse(player_cards[6]);
                 playerCard.GetComponent<PlayerCard>().AssignInfo();
             }
         }
@@ -49,4 +59,100 @@ public class Player : MonoBehaviour
             Debug.Log(getAllPlayerCardsRequest.error);
         }
     }
+
+    public void AttackCards()
+    {
+        DestroyAllPlayerCards();
+        StartCoroutine(GetAllAttackCards());
+    }
+
+    IEnumerator GetAllAttackCards()
+    {
+        WWWForm getAllAttackCardsForm = new WWWForm();
+        getAllAttackCardsForm.AddField("apppassword", "thisisfromtheapp!");
+        getAllAttackCardsForm.AddField("Id", CurrentPlayerId);
+        UnityWebRequest getAllAttackCardsRequest = UnityWebRequest.Post("http://localhost/playercards/getallattack.php", getAllAttackCardsForm);
+        yield return getAllAttackCardsRequest.SendWebRequest();
+        if (getAllAttackCardsRequest.error == null)
+        {
+            JSONNode allPlayerCards = JSON.Parse(getAllAttackCardsRequest.downloadHandler.text);
+            foreach (JSONNode player_cards in allPlayerCards)
+            {
+                var playerCard = Instantiate(playerCardPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                playerCard.transform.SetParent(panel.transform);
+                playerCard.GetComponent<PlayerCard>().cardname = player_cards[0];
+                playerCard.GetComponent<PlayerCard>().type = player_cards[1];
+                playerCard.GetComponent<PlayerCard>().description = player_cards[2];
+                playerCard.GetComponent<PlayerCard>().price = int.Parse(player_cards[3]);
+                playerCard.GetComponent<PlayerCard>().points = int.Parse(player_cards[4]);
+                playerCard.GetComponent<PlayerCard>().healthPoints = int.Parse(player_cards[5]);
+                playerCard.GetComponent<PlayerCard>().id = int.Parse(player_cards[6]);
+                playerCard.GetComponent<PlayerCard>().AssignInfo();
+            }
+        }
+        else
+        {
+            Debug.Log(getAllAttackCardsRequest.error);
+        }
+    }
+
+    public void DefenceCards()
+    {
+        DestroyAllPlayerCards();
+        StartCoroutine(GetAllDefenceCards());
+    }
+
+    IEnumerator GetAllDefenceCards()
+    {
+        WWWForm getAllDefenceCardsForm = new WWWForm();
+        getAllDefenceCardsForm.AddField("apppassword", "thisisfromtheapp!");
+        getAllDefenceCardsForm.AddField("Id", CurrentPlayerId);
+        UnityWebRequest getAllDefenceCardsRequest = UnityWebRequest.Post("http://localhost/playercards/getalldefence.php", getAllDefenceCardsForm);
+        yield return getAllDefenceCardsRequest.SendWebRequest();
+        if (getAllDefenceCardsRequest.error == null)
+        {
+            JSONNode allPlayerCards = JSON.Parse(getAllDefenceCardsRequest.downloadHandler.text);
+            foreach (JSONNode player_cards in allPlayerCards)
+            {
+                var playerCard = Instantiate(playerCardPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                playerCard.transform.SetParent(panel.transform);
+                playerCard.GetComponent<PlayerCard>().cardname = player_cards[0];
+                playerCard.GetComponent<PlayerCard>().type = player_cards[1];
+                playerCard.GetComponent<PlayerCard>().description = player_cards[2];
+                playerCard.GetComponent<PlayerCard>().price = int.Parse(player_cards[3]);
+                playerCard.GetComponent<PlayerCard>().points = int.Parse(player_cards[4]);
+                playerCard.GetComponent<PlayerCard>().healthPoints = int.Parse(player_cards[5]);
+                playerCard.GetComponent<PlayerCard>().id = int.Parse(player_cards[6]);
+                playerCard.GetComponent<PlayerCard>().AssignInfo();
+            }
+        }
+        else
+        {
+            Debug.Log(getAllDefenceCardsRequest.error);
+        }
+    }
+
+    public void DestroyAllPlayerCards()
+    {
+        var playerCards = GameObject.FindGameObjectsWithTag("PlayerCard");
+        foreach (var card in playerCards)
+        {
+            Destroy(card);
+        }
+    }
+
+    // continue
+    public void AddCard(){
+        StartCoroutine(AddCardToDeck());
+    }
+
+    IEnumerator AddCardToDeck()
+    {
+        WWWForm getAllDefenceCardsForm = new WWWForm();
+        getAllDefenceCardsForm.AddField("apppassword", "thisisfromtheapp!");
+        getAllDefenceCardsForm.AddField("Id", CurrentPlayerId);
+        UnityWebRequest getAllDefenceCardsRequest = UnityWebRequest.Post("http://localhost/playercards/getalldefence.php", getAllDefenceCardsForm);
+        yield return getAllDefenceCardsRequest.SendWebRequest();
+    }
+
 }
--- ARC2_Projekt/Assets/Scripts/Player/PlayerCard.cs
+++ ARC2_Projekt/Assets/Scripts/Player/PlayerCard.cs
@@ -10,12 +10,16 @@ public class PlayerCard : MonoBehaviour
     public Text Description;
     public Text Price;
     public Text Points;
+    public Text HealthPoints;
+    public Text Id;
 
     public string cardname;
     public string type;
     public string description;
     public int price;
     public int points;
+    public int healthPoints;
+    public int id;
 
     public void AssignInfo()
     {
@@ -24,6 +28,11 @@ public class PlayerCard : MonoBehaviour
         Description.text = description;
         Price.text = price.ToString();
         Points.text = points.ToString();
-
+        HealthPoints.text = healthPoints.ToString();
+    }
+    public void Swap()
+    {
+        Debug.Log(cardname + " - " + id);
+        // FindObjectOfType<Player>().AddCard();
     }
 }
